263167a849d69d830893ca345b2999d43a60a650
Ayoub Chaaoui <ayoubchaaoui02@gmail.com>
2026-02-16 21:54:30 +0000

    http init

--- cmd/taskmasterctl/main.go
+++ cmd/taskmasterctl/main.go
@@ -4,12 +4,14 @@ import (
 	"log/slog"
 
 	"github.com/ayb-cha/taskmaster/pkg/cli"
+	"github.com/ayb-cha/taskmaster/pkg/config"
 	"github.com/ayb-cha/taskmaster/pkg/logger"
 )
 
 func main() {
 	options := cli.InitControl()
 	logger.Init(options.LogLevel, &options.LogFilePath)
+	config := config.ReadConfig(options.ConfigPath)
 
-	slog.Debug("starting taskmasterctl")
+	slog.Debug("starting taskmasterctl", "config", config)
 }
--- pkg/cli/control-cli.go
+++ pkg/cli/control-cli.go
@@ -5,15 +5,19 @@ import "flag"
 type ControlOptions struct {
 	LogLevel    string
 	LogFilePath string
+	ConfigPath  string
 }
 
 func InitControl() ControlOptions {
 	logLevel := flag.String("log-level", "INFO", "Log level (DEBUG, INFO, WARN, ERROR)")
 	logFilePath := flag.String("log-file", "./logs/taskmasterctl.log", "Path to log file (optional)")
+	configPath := flag.String("conf", "./config/taskmaster.yaml", "Path to config file")
+
 	flag.Parse()
 
 	return ControlOptions{
 		LogLevel:    *logLevel,
 		LogFilePath: *logFilePath,
+		ConfigPath:  *configPath,
 	}
 }
